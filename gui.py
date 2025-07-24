e2347b59255080f8056b18266c330435403c153f
Martin Siegfried <siegfried.martin@yahoo.com>
2025-07-24 11:14:39 +0200

    Submenu einfügen

--- gui.py
+++ gui.py
@@ -28,6 +28,7 @@ class AppColors:
     SELECTION_BAR = '#eff5f6'
     TOP_FRAME = '#F5E1FD'
     SIDEBAR_FRAME = "#EEC6FF"
+    SUBMENU_FRAME = "#F5DDFF"
     HEADER = '#53366b'
     CONTENT_FRAME = "#ffffff"
     KU_COLOR = '#232F66'
@@ -105,124 +106,114 @@ class TopFrame(tk.Frame):
         # Aktualisiere die Uhr jede Sekunde
         self.after(1000, self.UpdateClock)
 
-class NavigationFrame(tk.Frame):
+class SubMenuFrame(tk.Frame):
     """
-    Seitenleiste mit Navigationsoptionen.
+    Rahmen für Submenü-Optionen, erscheint rechts neben dem Navigation Frame.
     """
     
-    def __init__(self, parent, content_updater):
-        """
-        Initialisiere die Navigationsleiste
+    def __init__(self, parent):
+        super().__init__(parent, bg=AppColors.SUBMENU_FRAME)
+        self.parent = parent
+        self.selected_submenu_item = None
+        self.submenu_labels = {}  # Speichert alle Submenu-Labels
         
-        Args:
-            parent: Das Elternelement
-            content_updater: Objekt mit update_content-Methode zur Aktualisierung des Inhalts
-        """
-        super().__init__(parent, bg=AppColors.SIDEBAR_FRAME)
-        self.content_updater = content_updater
-        self.nav_labels = {}
-        self.selected_option = None
-        
-        self._CreateHeader()
-        self._CreateNavigationOptions()
-    
-    def _CreateHeader(self):
-        """Erstellt die Navigationsüberschrift"""
-        self.navigation_header = tk.Label(
+        # Submenü-Header erstellen
+        self.submenu_header = tk.Label(
             self,
-            font=('Helvetica', 15, 'bold'),
-            bg=AppColors.SIDEBAR_FRAME,
+            font=('Helvetica', 12, 'bold'),
+            bg=AppColors.SUBMENU_FRAME,
             fg=AppColors.KU_COLOR,
-            text=AppConfig.NAVIGATION_TITLE
+            text="Unterkategorien"
         )
-        self.navigation_header.pack(side='top', padx=20, pady=10)
+        self.submenu_header.pack(side='top', fill='x', padx=10, pady=(10,5))
+        
+        # Frame für Submenü-Optionen
+        self.options_frame = tk.Frame(self, bg=AppColors.SUBMENU_FRAME)
+        self.options_frame.pack(fill='both', expand=True, padx=5)
+        
+        # Definiere verfügbare Submenüs
+        self.submenus = {
+            'Ansicht': ['Liste', 'Karte', 'Tabelle', 'Zeitstrahl'],
+            'Erstellung': ['Person', 'Ort', 'Ereignis', 'Quelle'],
+            'Export': ['PDF', 'CSV', 'Excel', 'XML'],
+            'Import': ['CSV-Import', 'XML-Import', 'Datenbank-Import'],
+            'BibTex': ['Literatur hinzufügen', 'Zitieren', 'Verwalten']
+        }
     
-    def _CreateNavigationOptions(self):
-        """Erstellt alle Navigationsoptionen"""
-        options_frame = tk.Frame(self, bg=AppColors.SIDEBAR_FRAME)
-        options_frame.pack(fill=tk.X, pady=2)
+    def UpdateSubmenu(self, option):
+        """Aktualisiert die Submenüoptionen für die gewählte Hauptoption"""
+        # Bestehende Optionen entfernen
+        for widget in self.options_frame.winfo_children():
+            widget.destroy()
         
-        # Erstelle für jede Option ein Label
-        for option in AppConfig.NAV_OPTIONS:
-            self._CreateNavOption(options_frame, option)  # Korrigierter Methodenname
+        # Zurücksetzen der Label-Sammlung
+        self.submenu_labels = {}
+        self.selected_submenu_item = None
         
-        # Wähle die erste Option standardmäßig aus
-        if AppConfig.NAV_OPTIONS:
-            self.OnSelect(AppConfig.NAV_OPTIONS[0])
+        # Header aktualisieren
+        self.submenu_header.config(text=f"{option}")
+        
+        # Neue Optionen hinzufügen
+        if option in self.submenus:
+            submenu_items = self.submenus[option]
+            for sub_option in submenu_items:
+                label = tk.Label(
+                    self.options_frame,
+                    text=sub_option,
+                    font=('Helvetica', 11),
+                    bg=AppColors.SUBMENU_FRAME,
+                    fg=AppColors.KU_COLOR,
+                    padx=15,
+                    pady=4,
+                    cursor="hand2",
+                    anchor='w'  # Linksbündiger Text
+                )
+                label.pack(fill='x', pady=1)
+                
+                # Event-Binding für das Label
+                label.bind("<Enter>", lambda e, l=label: self.OnSubMenuHover(l, True))
+                label.bind("<Leave>", lambda e, l=label: self.OnSubMenuHover(l, False))
+                label.bind("<Button-1>", lambda e, o=option, s=sub_option: self.OnSubMenuSelect(o, s))
+                
+                # Label speichern
+                self.submenu_labels[sub_option] = label
+            
+            # Automatisch das erste Element auswählen
+            if submenu_items:
+                first_option = submenu_items[0]
+                self.OnSubMenuSelect(option, first_option)
     
-    def _CreateNavOption(self, parent, option):  # Korrigierter Methodenname
-        """
-        Erstellt eine einzelne Navigationsoption
-        
-        Args:
-            parent: Das Elternelement für das Label
-            option: Der Text der Navigationsoption
-        """
-        label = tk.Label(
-            parent,
-            text=option,
-            font=('Helvetica', 12),
-            bg=AppColors.SIDEBAR_FRAME,
-            fg=AppColors.KU_COLOR,
-            padx=20,
-            pady=5,
-            cursor="hand2",  # Zeigt einen Hand-Cursor beim Hover
-            anchor='w'       # Linksbündiger Text
-        )
-        label.pack(fill=tk.X, pady=2)
-        
-        # Event-Binding für das Label
-        label.bind("<Enter>", lambda e, opt=option: self.OnHover(opt, True))
-        label.bind("<Leave>", lambda e, opt=option: self.OnHover(opt, False))
-        label.bind("<Button-1>", lambda e, opt=option: self.OnSelect(opt))
-        
-        # Label im Dictionary speichern
-        self.nav_labels[option] = label
-    
-    def OnHover(self, option, is_hover):
-        """
-        Behandelt Hover-Events für Navigationsoptionen
-        
-        Args:
-            option: Die Navigationsoption, über die gehovert wird
-            is_hover: True bei MouseEnter, False bei MouseLeave
-        """
-        label = self.nav_labels[option]
+    def OnSubMenuHover(self, label, is_hover):
+        """Behandelt Hover-Events für Submenüoptionen"""
+        sub_option = label.cget("text")
         # Nur hervorheben, wenn nicht ausgewählt
-        if option != self.selected_option:
+        if sub_option != self.selected_submenu_item:
             if is_hover:
                 label.configure(bg=AppColors.HOVER)
             else:
-                label.configure(bg=AppColors.SIDEBAR_FRAME)
+                label.configure(bg=AppColors.SUBMENU_FRAME)
     
-    def OnSelect(self, option):
-        """
-        Behandelt die Auswahl einer Navigationsoption
-        
-        Args:
-            option: Die ausgewählte Navigationsoption
-        """
+    def OnSubMenuSelect(self, option, sub_option):
+        """Behandelt die Auswahl einer Submenüoption"""
         # Zurücksetzen der vorherigen Auswahl
-        if self.selected_option:
-            old_label = self.nav_labels[self.selected_option]
+        if self.selected_submenu_item and self.selected_submenu_item in self.submenu_labels:
+            old_label = self.submenu_labels[self.selected_submenu_item]
             old_label.configure(
-                bg=AppColors.SIDEBAR_FRAME,
-                font=('Helvetica', 12)  # Normale Schrift
+                bg=AppColors.SUBMENU_FRAME,
+                font=('Helvetica', 11)  # Normale Schrift
             )
         
         # Neue Auswahl setzen
-        self.selected_option = option
-        selected_label = self.nav_labels[option]
-        selected_label.configure(
-            bg=AppColors.HOVER,
-            font=('Helvetica', 12, 'bold')  # Fettschrift
-        )
+        self.selected_submenu_item = sub_option
+        if sub_option in self.submenu_labels:
+            selected_label = self.submenu_labels[sub_option]
+            selected_label.configure(
+                bg=AppColors.HOVER,
+                font=('Helvetica', 11, 'bold')  # Fettschrift
+            )
         
         # Inhalt aktualisieren
-        self.content_updater.UpdateContent(option)
-        
-        # Ereignis protokollieren
-        logger.info(f"Navigation: Option '{option}' ausgewählt")
+        self.parent.HandleSubmenuSelect(option, sub_option)
 
 class ContentFrame(tk.Frame):
     """
@@ -253,16 +244,119 @@ class ContentFrame(tk.Frame):
         )
         label.pack(padx=50, pady=50)
 
+class NavigationFrame(tk.Frame):
+    """
+    Seitenleiste mit Navigationsoptionen.
+    """
+    
+    def __init__(self, parent, content_updater):
+        super().__init__(parent, bg=AppColors.SIDEBAR_FRAME)
+        self.content_updater = content_updater
+        self.nav_labels = {}
+        self.selected_option = None
+        
+        # Definiere, welche Optionen Submenus haben
+        self.options_with_submenu = ['Ansicht', 'Erstellung', 'Export', 'Import', 'BibTex']
+        
+        self._CreateHeader()
+        self._CreateNavigationOptions()
+    
+    def _CreateHeader(self):
+        """Erstellt die Navigationsüberschrift"""
+        self.navigation_header = tk.Label(
+            self,
+            font=('Helvetica', 15, 'bold'),
+            bg=AppColors.SIDEBAR_FRAME,
+            fg=AppColors.KU_COLOR,
+            text=AppConfig.NAVIGATION_TITLE
+        )
+        self.navigation_header.pack(side='top', padx=10, pady=10)  # Padding hinzugefügt
+    
+    def _CreateNavigationOptions(self):
+        """Erstellt alle Navigationsoptionen"""
+        # Container-Frame mit Padding
+        container_frame = tk.Frame(self, bg=AppColors.SIDEBAR_FRAME)
+        container_frame.pack(fill=tk.X, padx=5)  # Padding links und rechts
+        
+        options_frame = tk.Frame(container_frame, bg=AppColors.SIDEBAR_FRAME)
+        options_frame.pack(fill=tk.X, pady=2)
+        
+        # Erstelle für jede Option ein Label
+        for option in AppConfig.NAV_OPTIONS:
+            # Markiere Optionen mit Submenü
+            has_submenu = option in self.options_with_submenu
+            prefix = "» " if has_submenu else "  "
+            
+            label = tk.Label(
+                options_frame,
+                text=prefix + option,
+                font=('Helvetica', 12),
+                bg=AppColors.SIDEBAR_FRAME,
+                fg=AppColors.KU_COLOR,
+                padx=15,  # Reduziert von 20 auf 15 für konsistenteres Aussehen
+                pady=5,
+                cursor="hand2",
+                anchor='w'
+            )
+            label.pack(fill=tk.X, pady=2)
+            
+            # Event-Binding für das Label
+            label.bind("<Enter>", lambda e, opt=option: self.OnHover(opt, True))
+            label.bind("<Leave>", lambda e, opt=option: self.OnHover(opt, False))
+            label.bind("<Button-1>", lambda e, opt=option: self.OnSelect(opt))
+            
+            # Label im Dictionary speichern
+            self.nav_labels[option] = label
+    
+    def HasSubmenu(self, option):
+        """Prüft, ob eine Option ein Submenü hat"""
+        return option in self.options_with_submenu
+    
+    def OnHover(self, option, is_hover):
+        """Behandelt Hover-Events für Navigationsoptionen"""
+        label = self.nav_labels[option]
+        # Nur hervorheben, wenn nicht ausgewählt
+        if option != self.selected_option:
+            if is_hover:
+                label.configure(bg=AppColors.HOVER)
+            else:
+                label.configure(bg=AppColors.SIDEBAR_FRAME)
+    
+    def OnSelect(self, option):
+        """Behandelt die Auswahl einer Navigationsoption"""
+        # Zurücksetzen der vorherigen Auswahl
+        if self.selected_option:
+            old_label = self.nav_labels[self.selected_option]
+            prefix = "» " if self.HasSubmenu(self.selected_option) else "  "
+            old_label.configure(
+                bg=AppColors.SIDEBAR_FRAME,
+                font=('Helvetica', 12),
+                text=prefix + self.selected_option
+            )
+        
+        # Neue Auswahl setzen
+        self.selected_option = option
+        selected_label = self.nav_labels[option]
+        prefix = "» " if self.HasSubmenu(option) else "  "
+        selected_label.configure(
+            bg=AppColors.HOVER,
+            font=('Helvetica', 12, 'bold'),
+            text=prefix + option
+        )
+        
+        # Inhalt aktualisieren
+        self.content_updater.UpdateContent(option)
+        
+        # Ereignis protokollieren
+        logger.info(f"Navigation: Option '{option}' ausgewählt")
+
+
 class MainApp(tk.Tk):
     """
     Hauptanwendungsklasse für Prosop.
-    
-    Verantwortlich für die Erstellung und Organisation aller UI-Komponenten
-    und die Steuerung des Anwendungsflusses.
     """
     
     def __init__(self):
-        """Initialisiert die Hauptanwendung und alle Komponenten"""
         super().__init__()
         
         # --- Fensterkonfiguration ---
@@ -280,11 +374,20 @@ class MainApp(tk.Tk):
         # --- UI-Komponenten erstellen ---
         self._CreateUiComponents()
         
+        # Erstes Navigation-Element direkt auswählen
+        if len(AppConfig.NAV_OPTIONS) > 0:
+            self.navigation_frame.OnSelect(AppConfig.NAV_OPTIONS[0])
+        
         # Anwendungsstart protokollieren
         logger.info("Anwendung gestartet")
     
+    def _SelectInitialNavigation(self):
+        """Wählt das erste Navigationselement aus"""
+        if len(AppConfig.NAV_OPTIONS) > 0:
+            logger.info(f"Automatische Auswahl des ersten Elements: {AppConfig.NAV_OPTIONS[0]}")
+            self.navigation_frame.OnSelect(AppConfig.NAV_OPTIONS[0])
+    
     def _SetupIcon(self):
-        """Richtet das Anwendungsicon ein"""
         try:
             self.icon = tk.PhotoImage(file=AppConfig.APP_ICON)
             self.iconphoto(True, self.icon)
@@ -293,33 +396,50 @@ class MainApp(tk.Tk):
     
     def _ConfigureMainGrid(self):
         """Konfiguriert das Grid-Layout des Hauptfensters"""
-        self.columnconfigure(0, weight=0)  # Navigationsspalte (feste Breite)
-        self.columnconfigure(1, weight=1)  # Inhaltsspalte (dehnbar)
-        self.rowconfigure(0, weight=0)     # Obere Zeile (feste Höhe)
-        self.rowconfigure(1, weight=1)     # Inhaltszeile (dehnbar)
+        self.columnconfigure(0, weight=0)   # Navigationsspalte (feste Breite)
+        self.columnconfigure(1, weight=0)   # Submenüspalte (passt sich an Inhalt an)
+        self.columnconfigure(2, weight=1)   # Inhaltsspalte (dehnbar)
+        self.rowconfigure(0, weight=0)      # Obere Zeile (feste Höhe)
+        self.rowconfigure(1, weight=1)      # Inhaltszeile (dehnbar)
     
     def _CreateUiComponents(self):
         """Erstellt alle UI-Komponenten der Anwendung"""
         # Oberen Rahmen erstellen
         self.top_frame = TopFrame(self)
-        self.top_frame.grid(row=0, column=0, columnspan=2, sticky='nsew')
+        self.top_frame.grid(row=0, column=0, columnspan=3, sticky='nsew')
         
-        # Inhaltsrahmen erstellen (vor der Navigation, damit er referenziert werden kann)
+        # Inhaltsrahmen erstellen
         self.content_frame = ContentFrame(self)
-        self.content_frame.grid(row=1, column=1, sticky='nsew')
+        self.content_frame.grid(row=1, column=2, sticky='nsew', padx=(5, 5), pady=(5, 5))
+        
+        # Submenü-Rahmen erstellen
+        self.submenu_frame = SubMenuFrame(self)
+        # Nicht direkt anzeigen bis ein Element mit Submenü ausgewählt wird
         
         # Navigationsrahmen erstellen und mit dem Inhaltsrahmen verbinden
         self.navigation_frame = NavigationFrame(self, self)
         self.navigation_frame.grid(row=1, column=0, sticky='nsew')
     
     def UpdateContent(self, option):
-        """
-        Aktualisiert den Inhaltsbereich basierend auf der Navigationsoption
-        
-        Args:
-            option: Die ausgewählte Navigationsoption
-        """
-        self.content_frame.UpdateContent(option)  # Auch hier konsistente Benennung
+        """Aktualisiert den Inhaltsbereich basierend auf der Navigationsoption"""
+        # Prüfe, ob die Option ein Submenü hat
+        if self.navigation_frame.HasSubmenu(option):
+            # Zeige Submenü an
+            self.submenu_frame.grid(row=1, column=1, sticky='nsew')
+            self.submenu_frame.UpdateSubmenu(option)
+            # Kein rechter Rand für das Navigationsmenu nötig, wenn Submenu angezeigt wird
+            self.navigation_frame.grid_configure(padx=(0, 0))
+        else:
+            # Verstecke Submenü
+            self.submenu_frame.grid_forget()
+            # Aktualisiere den Inhalt direkt
+            self.content_frame.UpdateContent(option)
+    
+    def HandleSubmenuSelect(self, main_option, submenu_option):
+        """Behandelt die Auswahl einer Submenüoption"""
+        # Verstecke das Submenü nicht, damit es sichtbar bleibt
+        # Aktualisiere nur den Inhalt
+        self.content_frame.UpdateContent(f"{main_option} - {submenu_option}")
 
 if __name__ == '__main__':
     # Anwendung erstellen und starten
